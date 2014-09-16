e1fe70ba9cdd869faff0344305bf106565d49c7a
Ken Ohata <ohata@daraf.co.jp>
2014-09-16 23:01:14 +0900

    add application#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @yaml = YAML.load_file("#{Rails.root}/app/models/concerns/default.yml")
+  end
 end
