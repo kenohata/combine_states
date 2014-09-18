e7dd51f6a45f43732e270cde68f9e7e5ad6e3aea
Ken Ohata <ohata@daraf.co.jp>
2014-09-19 00:24:26 +0900

    fix UploadedYAML

--- app/models/uploaded_yaml.rb
+++ app/models/uploaded_yaml.rb
@@ -3,6 +3,28 @@ class UploadedYAML
   attr_accessor :file
 
   def yaml
-    @yaml = YAML.load_file(@file.absolute_path)
+    @yaml = YAML.load_file(@file.path)
+  end
+
+  def value_array?
+    yaml.map{|k, v| Array === v }.all?
+  end
+
+  def value_hash?
+    yaml.map{|k, v| Hash === v }.all?
+  end
+
+  def sub_value_array?
+    if value_hash?
+      yaml.map{|k, v| v.map{|k, v| Array === v }.flatten }.all?
+    end
+  end
+
+  def single_model?
+    value_array?
+  end
+
+  def multi_model?
+    (! value_array?) and value_hash? and sub_value_array?
   end
 end
