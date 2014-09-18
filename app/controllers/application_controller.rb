f6ab1c533d167e0676fc985d03d085ddebe1b434
Ken Ohata <ohata@daraf.co.jp>
2014-09-18 23:35:00 +0900

    update application#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,53 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    @yaml = YAML.load_file("#{Rails.root}/app/models/concerns/default.yml")
+    @tree = YAMLtree.load_file("#{Rails.root}/app/models/concerns/default.yml")
+  end
+
+  class YAMLtree
+    include Enumerable
+
+    def self.load_file path
+      tree = self.new
+      tree.load_file path
+      tree
+    end
+
+    def load_file path
+      @yaml = YAML.load_file path
+    end
+
+    def keys
+      @yaml.keys
+    end
+
+    def next_key_by key
+      index = nil
+
+      keys.each_with_index do |k, i|
+        if key == k
+          index = i
+          break
+        end
+      end
+
+      keys[index + 1]
+    end
+
+    def each_inside key
+      @yaml[key].each do |state|
+        if next_key = next_key_by(key)
+          each_inside(next_key) { |s| yield ([state] << s).flatten }
+        else
+          yield state
+        end
+      end
+    end
+
+    def each
+      each_inside(keys.first) do |state|
+        yield state
+      end
+    end
   end
 end
