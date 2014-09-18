142428f2c086ffb0a3cc9242b53b715a26a00a53
Ken Ohata <ohata@daraf.co.jp>
2014-09-19 00:24:09 +0900

    ailgnment

--- app/controllers/uploaded_yamls_controller.rb
+++ app/controllers/uploaded_yamls_controller.rb
@@ -1,14 +1,14 @@
 class UploadedYamlsController < ApplicationController
   def show
-    @file     = File.open("#{Rails.root}/app/models/concerns/default.yml")
-    @tree     = YAMLtree.load_file @file.path
-    @uploaded = UploadedYAML.new file: @file
+    @file = File.open("#{Rails.root}/app/models/concerns/default.yml")
+    @yaml = UploadedYAML.new file: @file
+    @tree = YAMLtree.load_file @file.path
   end
 
   def update
-    @file     = params[:uploaded_yaml][:file]
-    @tree     = YAMLtree.load_file @file.path
-    @uploaded = UploadedYAML.new file: @file
+    @file = params[:uploaded_yaml][:file]
+    @yaml = UploadedYAML.new file: @file
+    @tree = YAMLtree.load_file @file.path
 
     render :show
   end
